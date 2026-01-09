5e3b45c65da5c3d2e2c568ba57ba955d38ae2b19
DanielAHB1 <00036225@uca.edu.sv>
2026-01-09 17:54:27 -0600

    add: codigo base

---
+++ calculadora.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x1, x2, op, res;
+
+    cout<<"Bienvenido, elige la operacion que quieres realizar "<< endl;
+    cout<<"1. Multiplicacion "<< endl;
+    cout<<"2. Division "<< endl;
+    cout<<"3. Suma "<< endl;
+    cout<<"4. Resta "<< endl;
+    cout<<"Su opcion es: "; 
+    cin >> op;
+
+}
